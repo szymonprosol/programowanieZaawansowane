7e3b851029c2f5d7316ba8ed5050ca3242728e83
Szymon Rosół <81100512+szymonprosol@users.noreply.github.com>
2022-11-20 19:57:58 +0100

    first commit

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Component;
 @Getter
 public class AdminConfig {
 
-    @Value("${ADMIN_MAIL}")
+    @Value("szymonkodilla@gmail.com")
     private String adminMail;
 }
