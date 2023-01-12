54d083763c6aafe8d962973c52972fae816d42ea
Szymon Rosół <81100512+szymonprosol@users.noreply.github.com>
2023-01-12 20:36:03 +0100

    Changes

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,26 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.CreatedTrelloCardDto;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class TrelloService {
+    public static final String SUBJECT = "Tasks: New Trello card";
+    private final TrelloClient trelloClient;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCardDto createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
+        return newCard;
+    }
+}
