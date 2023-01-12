0d2d4ab5f1818e5bb24423851e1e451d84c4ab08
Szymon Rosół <81100512+szymonprosol@users.noreply.github.com>
2023-01-12 22:51:19 +0100

    Changes

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,29 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.CreatedTrelloCardDto;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.service.TrelloService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class TrelloController {
+
+    private final TrelloService trelloService;
+
+    @GetMapping("getTrelloBoards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloService.fetchTrelloBoards();
+    }
+
+    @PostMapping("createTrelloCard")
+    public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloService.createTrelloCard(trelloCardDto);
+    }
+}
---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,27 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloBoardDto {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("lists")
+    private List<TrelloListDto> lists;
+
+}
